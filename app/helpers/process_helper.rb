ab9443e4962a6a4a62e6235565a6b25c7cbc699e
Jeffrey Tong <trigun0x2@gmail.com>
2013-04-02 19:42:58 -0400

    it now renders a site using readbility api

--- app/helpers/process_helper.rb
+++ app/helpers/process_helper.rb
@@ -1,10 +1,9 @@
 module ProcessHelper
 	
-	def noko
-		require 'rubygems'
-		require 'nokogiri'
-		require 'open-uri'
-		page = Nokogiri::HTML(open("http://en.wikipedia.org/"))
-		puts page.class
+	def read
+		Readit::API.new 'trigun0x2','TcjVwsvrsTTsxcXp3Xzs6ZVqbmUssYNd'
+		@parser = Readit::Parser.new "1e6ede01e61c2c1de9b740d9d4f53857f4c9a2ba"
+		@stuff=@parser.parse "http://ruby.railstutorial.org/chapters/updating-showing-and-deleting-users"
+		@stuff.content
 	end
 end
